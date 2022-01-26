ba426f870178e12698fd2f887355c55fcc1bd65f
Joel Nowicki <jay_leno@gmx.de>
2022-01-26 17:20:21 +0100

    conditional-rendering

--- src/components/VaccinationList.jsx
+++ src/components/VaccinationList.jsx
@@ -2,9 +2,10 @@ import styled from "styled-components";
 import { useState, useEffect } from "react";
 import GlobalStyles, { Button } from "../GlobalStyles/GlobalStyles";
 import { saveToLocal, loadFromLocal } from "../lib/localStorage";
+import Vaccination from "../Images/Vaccination.jpg";
 
 function VaccinationList() {
-  const initialValues = {
+  const initialVaccinations = {
     hepatitis: {
       value: "Hepatitis",
       checked: false,
@@ -18,130 +19,84 @@ function VaccinationList() {
       value: "Parainfluenza",
       checked: false,
     },
-    
-    pavovirose:{
-      
+
+    pavovirose: {
       value: "Pavovirose",
       checked: false,
     },
 
-    staupe:{
-      
+    staupe: {
       value: "Staupe",
       checked: false,
     },
 
-    tollwut:{
-    
+    tollwut: {
       value: "Tollwut",
       checked: false,
     },
-   
   };
 
-  const [isChecked, setIsChecked] = useState(initialValues);
+  const localStorageVaccinations = loadFromLocal("_vaccinations");
+  const [vaccination, setVaccination] = useState(
+    localStorageVaccinations ?? initialVaccinations
+  );
 
-  const vaccinationKeys = Object.keys(initialValues);
-  // Object.keys(initialValues).map(vaccinationKey => initialValues[vaccinationKey])
-  
+  const vaccinationKeys = Object.keys(initialVaccinations);
+  const [showMore, setShowMore] = useState(false);
+
   const handleChange = (event) => {
     let inputValue = event.target.checked;
     let vaccinationKey = event.target.value.toLowerCase();
-    setIsChecked({
-      ...isChecked,
-      [vaccinationKey]: { ...isChecked[vaccinationKey], checked: inputValue },
+    setVaccination({
+      ...vaccination,
+      [vaccinationKey]: { ...vaccination[vaccinationKey], checked: inputValue },
     });
   };
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    saveToLocal("v", isChecked);
+    saveToLocal("_vaccinations", vaccination);
   };
-  
-  console.log(isChecked);
-  
-  const checkboxes = vaccinationKeys.map((key) => (
-    <label>
-      <input
-        type="checkbox"
-        value={isChecked[key]["value"]}
-        name="vaccination"
-        checked={isChecked[key]["checked"]}
-        onChange={handleChange}
-      />
-      {isChecked[key]["value"]}
-    </label>
-  ));
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
   return (
     <>
-      <Container onSubmit={handleSubmit}>
-        {checkboxes}
-        <article>
-          <input
-            name="vaccination"
-            type="checkbox"
-            value="Hepatitis"
-            onChange={handleChange}
-            checked={isChecked.checked}
-            
-          />
-          <label>Hepatitis</label>
-        </article>
-        <article>
-          <input
-            name="vaccination"
-            type="checkbox"
-            value="Leptospirose"
-            onChange={handleChange}
-            checked={isChecked.checked}
-          />
-          <label>Leptospirose</label>
-        </article>
-        <article>
-          <input
-            name="vaccination"
-            type="checkbox"
-            value="Parainfluenza"
-            onChange={handleChange}
-            checked={isChecked.checked}
-          />
-          <label>Parainfluenza</label>
-        </article>
-        <article>
-          <input
-            name="vaccination"
-            type="checkbox"
-            value="Pavovirose"
-            onChange={handleChange}
-            checked={isChecked.checked}
-          />
-          <label>Pavovirose</label>
-        </article>
-        <article>
-          <input
-            name="vaccination"
-            type="checkbox"
-            value="Staupe"
-            onChange={handleChange}
-            checked={isChecked.checked}
-          />
-          <label>Staupe</label>
-        </article>
-        <article>
-          <input
-            name="vaccination"
-            type="checkbox"
-            value="Tollwut"
-            onChange={handleChange}
-            checked={isChecked.checked}
-          />
-          <label>Tollwut</label>
-        </article>
-
-        <ButtonBox>
-          <Button type="submit">Speichern</Button>
-        </ButtonBox>
-      </Container>
+      <TextBox>
+        <img src={Vaccination} alt="" />
+        <span>
+          Gefährliche Infektionskrankheiten sind bei Hunden nach wie vor weit
+          verbreitet. Impfungen ermöglichen es, deinen Liebling vor diesen
+          Erkrankungen zu schützen oder zumindest zu verhindern, dass diese
+          einen schweren Verlauf haben.
+        </span>
+      </TextBox>
+      <ButtonBox>
+        <Button onClick={handleMoreClick}>
+          {showMore ? "schließen" : "Meine Impfungen"}
+        </Button>
+      </ButtonBox>
+      {showMore && (
+        <Container onSubmit={handleSubmit}>
+          {vaccinationKeys.map((key, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                value={vaccination[key]["value"]}
+                name="vaccination"
+                checked={vaccination[key]["checked"]}
+                onChange={handleChange}
+              />
+              {vaccination[key]["value"]}
+            </label>
+          ))}
+          <ButtonBox>
+            <Button type="submit">Speichern</Button>
+          </ButtonBox>
+        </Container>
+      )}
     </>
   );
 }
@@ -149,21 +104,23 @@ function VaccinationList() {
 export default VaccinationList;
 
 const Container = styled.form`
-  width: 80%;
-
+  width: 22rem;
   margin: auto;
-  margin-top: 6rem;
+  margin-top: 2rem;
   display: flex;
   flex-direction: column;
-  gap: 2.5rem;
+  gap: 2.7rem;
 
   input {
-    margin-left: 5rem;
+    width: 20px;
+    height: 20px;
+    margin-right: 1rem;
     accent-color: var(--primary-one);
   }
+
   label {
-    margin-left: 2rem;
-    font-size: 1.5rem;
+    margin-left: 6rem;
+    font-size: 1.7rem;
     font-family: "Raleway", sans-serif;
     font-weight: 600;
     color: var(--secondary-one);
@@ -171,4 +128,27 @@ const Container = styled.form`
 `;
 const ButtonBox = styled.div`
   display: flex;
+  margin-bottom: 2rem;
+`;
+const TextBox = styled.div`
+  img {
+    width: 21.5rem;
+    margin-top: 0.5rem;
+    -webkit-box-shadow: 0 10px 6px -6px #777;
+    -moz-box-shadow: 0 10px 6px -6px #777;
+    box-shadow: 0 10px 6px -6px #777;
+  }
+  span {
+    margin-top: 1rem;
+    text-align: justify;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  line-height: 1.6rem;
+  margin-bottom: 1.3rem;
+  font-size: 1.1rem;
+  font-weight: 300;
+  color: var(--secondary-one);
 `;
